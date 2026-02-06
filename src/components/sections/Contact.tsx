3a6f6a6a8d006f4d868bf51a87a6259d5abd0879
Colby Nelsen <colby.nelsen@gmail.com>
2026-02-06 03:21:17 -0800

    fix: align Netlify forms detector path
    - Rename detector to public/_forms.html
    - Update contact form action to /_forms.html
    - Replace remaining __forms.html references

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -123,7 +123,7 @@ export default function Contact() {
             <form
               name="quote"
               method="POST"
-              action="/__forms.html"
+              action="/_forms.html"
               data-netlify="true"
               data-netlify-honeypot="bot-field"
               className="grid gap-4"
